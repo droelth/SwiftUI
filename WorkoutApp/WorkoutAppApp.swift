330e1e7939b3518a274ebc8379c962e7e1736519
Berkay <berkaysutlu94@gmail.com>
2023-06-19 00:55:55 +0300

    Initial Commit

---
+++ WorkoutApp/WorkoutAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutAppApp.swift
+//  WorkoutApp
+//
+//  Created by Berkay Sutlu on 19.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
